309cd561ee46dafe98516c27e1013901914a98d8
Raniere Prates <rsilva@uplexis.com.br>
2021-02-17 12:35:38 -0300

    criei estrutura inicial das rotas

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\BarberController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +17,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('auth/login', [AuthController::class, 'login']);
+Route::post('auth/logout', [AuthController::class, 'logout']);
+Route::post('auth/refresh', [AuthController::class, 'refresh']);
+
+Route::post('user/store', [UserController::class, 'store']);
+Route::get('user', [UserController::class, 'read']);
+Route::put('user/{id}', [UserController::class, 'update']);
+Route::get('user/favorites', [UserController::class, 'getFavorites']);
+Route::post('user/favorite', [UserController::class, 'addFavorite']);
+Route::post('user/appointments', [UserController::class, 'getAppointments']);
+
+Route::get('barbers/index', [BarberController::class, 'index']);
+Route::get('barber/{id}', [BarberController::class, 'show']);
+Route::post('barber/{id}/appointment', [BarberController::class, 'setAppointment']);
+
+
+Route::get('barber/search', [BarberController::class, 'search']);
