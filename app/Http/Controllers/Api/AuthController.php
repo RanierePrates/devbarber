f9a1c05770e63a9bcfcc923abc8f9e60ac527e68
Raniere Prates <rsilva@uplexis.com.br>
2021-02-17 15:31:53 -0300

    criei login

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,10 +3,38 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\AuthRequest;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
+/**
+ * Class AuthController
+ * @package App\Http\Controllers\Api
+ */
 class AuthController extends Controller
 {
+
+    /**
+     * @param AuthRequest $request
+     * @return JsonResponse
+     */
+    public function login(AuthRequest $request): JsonResponse
+    {
+        $token = auth()->attempt([
+            'email' => $request->get('email'),
+            'password' => $request->get('password'),
+        ]);
+
+        if (!$token) {
+            return response()->json(['Usuário e/ou senha inválido!']);
+        }
+
+        return response()->json([
+            'data' => auth()->user(),
+            'token' => $token
+        ]);
+    }
+
     /**
      * Display a listing of the resource.
      *
